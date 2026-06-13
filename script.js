a0de56eb24dd9a9dc44c5dcf1b05fb7bcef714ed
trexxxxxxxx <tomas.t@tumo.world>
2026-06-13 16:45:11 -0300

    Segundo Commit 13/06/2026

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function calculo() {
     if (cantNum > 2) {
         tipoOperacion = parseInt(prompt("Como es un cálculo de más de dos números, ingrese 1 para Suma/Resta, o 2 para Multiplicación/División"));
     }
-    else {
+    else if (cantNum == 2) {
         tipoOperacion = 3;
     }
+    else {
+        console.log("Algo salió mal")
+        return;
+    }
+
     
     const numeros = [];
     const operaciones = [];
@@ -16,8 +21,16 @@ function calculo() {
     for (let i = 0; i < cantNum; i++) {
         a = aAnterior;
         numeros[i] = parseFloat(prompt("Escribe el número número " + [i + 1] + " de tu operación matemática"));
+        while (Number.isNaN(numeros[i])) {
+            alert("Solo se pueden ingresar valores numéricos. Intentelo de nuevo a continuación")
+            numeros[i] = parseFloat(prompt("Escribe el número número " + [i + 1] + " de tu operación matemática"));
+        }
         while (a + 1 != cantNum) {
-            operaciones[a] = parseInt(prompt("Escribe el tipo de operación: 1. Suma, 2. Resta, 3. Multiplicación, 4. División"));
+            operaciones[a] = prompt("Escribe el tipo de operación: + - * /");
+            while (operaciones[a] !== "+" && operaciones[a] !== "-" && operaciones[a] !== "*" && operaciones[a] !== "/") {
+                alert("Solo se pueden usar los operadores + - * /. Intentelo de nuevo a continuación")
+                operaciones[a] = prompt("Escribe el tipo de operación: + - * /");
+            }
             a++;
             aAnterior = a;
             a = cantNum - 1;
@@ -28,42 +41,45 @@ function calculo() {
     let u = numeros.length;
     for (let n = 1; n != u; n++) {
         if (tipoOperacion == 1) {
-            if (operaciones[n-1] == 1) {
+            if (operaciones[n-1] == "+") {
                 resultado = resultado + numeros[n];
             }
-            else if (operaciones[n-1] == 2) {
+            else if (operaciones[n-1] == "-") {
                 resultado = resultado - numeros[n];
             }
             else {
                 alert("Algo salió mal");
+                return;
             }
         }
         else if (tipoOperacion == 2) {
-            if (operaciones[n-1] == 3) {
+            if (operaciones[n-1] == "*") {
                 resultado = resultado * numeros[n];
             }
-            else if (operaciones[n-1] == 4) {
+            else if (operaciones[n-1] == "/") {
                 resultado = resultado / numeros[n];
             }
             else {
                 alert("Algo salió mal");
+                return;
             }
         }
         else if (tipoOperacion == 3) {
-            if (operaciones[n-1] == 1) {
+            if (operaciones[n-1] == "+") {
                 resultado = resultado + numeros[n];
             }
-            else if (operaciones[n-1] == 2) {
+            else if (operaciones[n-1] == "-") {
                 resultado = resultado - numeros[n];
             }
-            else if (operaciones[n-1] == 3) {
+            else if (operaciones[n-1] == "*") {
                 resultado = resultado * numeros[n];
             }
-            else if (operaciones[n-1] == 4) {
+            else if (operaciones[n-1] == "/") {
                 resultado = resultado / numeros[n];
             }
             else {
                 alert("Algo salió mal");
+                return;
             }
         }   
     }
